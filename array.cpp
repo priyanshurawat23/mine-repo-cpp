e7cddac645f7c8fb1a2d9dd9384ef34a0fe17b89
Priyanshu Rawat <priyanshu.r2309@gmail.com>
2025-11-10 12:20:58 +0530

    Add files via upload

---
+++ array.cpp
@@ -0,0 +1,198 @@
+// // wap to input a 1-dimensional array and also print it.
+// // wap to find out the addition of two arrays.
+// // wap to find the multiplication of two arrays/
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     int p[6],a;
+//     for(a=0;a<6;a+=1){
+//         cin>>p[a];
+        
+//     }
+//     for(a=0;a<6;a+=1){
+//         cout<<p[a]<<", ";
+//     }
+
+// }
+
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+// 	int a[5],b[5],p;
+// 	for(p=0;p<5;p+=1){
+// 	    cin>>a[p];
+	    
+// 	}
+// 	for(p=0;p<5;p+=1){
+// 	    cin>>b[p];
+	    
+// 	}
+// 	for(p=0;p<5;p+=1){
+// 	    cout<<a[p]+b[p]<<", ";
+// 	}
+
+// }
+
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+// 	int a[5],b[5],p;
+// 	for(p=0;p<5;p+=1){
+// 	    cin>>a[p];
+	    
+// 	}
+// 	for(p=0;p<5;p+=1){
+// 	    cin>>b[p];
+	
+	    
+// 	}
+	
+// 	for(p=0;p<5;p+=1){
+// 	    cout<<a[p]*b[p]<<", ";
+// 	}
+	
+
+// }
+
+// // wap to find out the addition of all the numbers of an array.
+// // wap to find the addtion of all odd no and all even no of an array.
+// // wap to find addition of all even index values and all odd index values.
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+// 	int a[5],p,s;
+// 	s=0;
+// 	for(p=0;p<5;p+=1){
+// 	    cin>>a[p];
+	    
+// 	}
+// 	for(p=0;p<5;p+=1){
+// 	    s=s+a[p];
+	    
+// 	}
+// 	cout<<s;
+
+// }
+
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     int a[5],p,o,e;
+//     o=0;
+//     e=0;
+    
+//     for(p=0;p<5;p+=1){
+// 	    cin>>a[p];
+	    
+// 	}
+// 	for(p=0;p<5;p+=1){
+// 	    if((a[p]%2)==0){
+// 	        e+=a[p];
+// 	    }
+// 	    if((a[p]%2)!=0){
+// 	        o+=a[p];
+	        
+// 	    }
+// 	}
+// 	cout<<"odd "<<o;
+// 	cout<<"even "<<e;
+
+// }
+
+
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+// 	int a[5],p,o,e;
+// 	o=0;
+// 	e=0;
+	
+// 	for(p=0;p<5;p+=1){
+// 	    cin>>a[p];
+	    
+// 	}
+// 	for(p=0;p<5;p+=1){
+// 	    if((p%2)==0){
+// 	        e+=a[p];
+	        
+// 	    }
+// 	    else{
+// 	        o+=a[p];
+// 	    }
+// 	}
+// 	cout<<"odd "<<o;
+// 	cout<<"even "<<e;
+
+	
+
+// }
+
+
+
+// // wap to find the biggest number pf an array.
+// // wap to find the smallest no. of an array.
+
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+// 	int a[5],p,b;
+// 	for(p=0;p<5;p+=1){
+// 	    cin>>a[p];
+	    
+// 	}
+// 	b=a[0];
+// 	for (p=0;p<5;p+=1){
+// 	    if(a[p]>b){
+// 	        b=a[p];
+	        
+// 	    }
+	    
+// 	}
+// 	cout<<b;
+
+// }
+
+
+
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+// 	int a[5],p,b;
+// 	for(p=0;p<5;p+=1){
+// 	    cin>>a[p];
+	    
+// 	}
+// 	b=a[0];
+// 	for (p=0;p<5;p+=1){
+// 	    if(a[p]<b){
+// 	        b=a[p];
+	        
+// 	    }
+	    
+// 	}
+// 	cout<<b;
+
+// }
